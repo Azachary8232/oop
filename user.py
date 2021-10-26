0d74903286bf8c3939fd70984083d0b7ab10fc6d
Azachary8232 <90228565+Azachary8232@users.noreply.github.com>
2021-10-25 22:00:01 -0600

    Chaining Methods Complete

--- user.py
+++ user.py
@@ -5,12 +5,15 @@ class User:
 
     def make_withdrawal(self,amount):
         self.account_balance -= amount
-    
+        return self
+
     def display_user_balance(self):
         print(self.name, self.account_balance)
+        return self
 
     def make_deposit(self, amount):
         self.account_balance += amount
+        return self
 
     def make_transfer(self, amount, to_whom):
         self.account_balance -= amount
@@ -22,23 +25,11 @@ p1 = User("John")
 p2 = User("Mark")
 p3 = User("Mary")
 
-p1.make_deposit(250)
-p1.make_deposit(745)
-p1.make_deposit(1025.65)
-p1.make_deposit(62.47)
-p1.display_user_balance()
+p1.make_deposit(250).make_deposit(745).make_deposit(1025.65).make_deposit(62.47).display_user_balance()
 
-p2.make_deposit(8521.65)
-p2.make_deposit(45)
-p2.make_withdrawal(651)
-p2.make_withdrawal(219.25)
-p2.display_user_balance()
+p2.make_deposit(8521.65).make_deposit(45).make_withdrawal(651).make_withdrawal(219.25).display_user_balance()
 
-p3.make_deposit(455)
-p3.make_withdrawal(212.25)
-p3.make_withdrawal(168.96)
-p3.make_withdrawal(201.10)
-p3.display_user_balance()
+p3.make_deposit(455).make_withdrawal(212.25).make_withdrawal(168.96).make_withdrawal(201.10).display_user_balance()
 
 p1.make_transfer(500,p3)
 p1.display_user_balance()
