6430886520d5a7574ec91778ae05b7895de184a4
Azachary8232 <90228565+Azachary8232@users.noreply.github.com>
2021-10-26 21:29:11 -0600

    Dojo Pets Setup

---
+++ dojo_pets.py
@@ -0,0 +1,30 @@
+class Ninja:
+
+    def __init__(self, first_name, last_name, pet, treats, pet_food) -> None:
+        pass
+
+    def walk(self):
+        pass
+
+    def feed(self):
+        pass
+
+    def bathe(self):
+        pass
+
+class Pet:
+
+    def __init__(self, name, type, tricks, health, energy) -> None:
+        pass
+
+    def sleep(self):
+        pass
+
+    def eat(self):
+        pass
+
+    def play(self):
+        pass
+
+    def noise(self):
+        pass
