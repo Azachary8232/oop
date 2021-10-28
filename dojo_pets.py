885e48e54510391c4dafbf7f2e4abcd3c9b973f6
Azachary8232 <90228565+Azachary8232@users.noreply.github.com>
2021-10-27 18:01:24 -0600

    Dojo Pet Active

--- dojo_pets.py
+++ dojo_pets.py
@@ -1,45 +1,55 @@
 class Ninja:
 
-    def __init__(self, first_name, last_name, pet, treats, pet_food) -> None:
+    def __init__(self, first_name, last_name, pet, treats, pet_food):
         self.name = first_name + last_name
         self.pet = pet
         self.pet_treats = treats
         self.pet_food = pet_food
 
     def walk(self):
-        pass
+        self.pet.play()
+        return self
 
     def feed(self):
-        pass
+        self.pet.eat()
+        return self
 
     def bathe(self):
-        pass
+        self.pet.noise()
+        return self
 
 class Pet:
 
-    def __init__(self, name, type, tricks, health, energy) -> None:
+    def __init__(self, name, type, tricks):
         self.pet_name = name
         self.pet_type = type
         self.trick_type = tricks
-        self.pet_health = health
-        self.pet_energy = energy
+        self.pet_health = 0
+        self.pet_energy = 0
 
     def sleep(self):
-        pass
+        self.pet_energy += 25
+        return self
 
     def eat(self):
-        pass
+        self.pet_energy += 5
+        self.pet_health += 10
+        return self
 
     def play(self):
-        pass
+        self.pet_health += 5
+        return self
 
     def noise(self):
-        pass
+        print(f"your {self.pet_type} made whatever noise a {self.pet_type} makes.")
+        return self
 
 
-splinter = Ninja("Master ", "Splinter", "Donatello", "carrots", "pizza")
-donatello = Pet("Donatello", "turtle", "Bo Staff", "super", "ninja")
+donatello = Pet("Donatello", "turtle", "Bo Staff",)
+splinter = Ninja("Master ", "Splinter", donatello, "carrots", "pizza")
 
 
-print(splinter.pet.pet_name)
+splinter.feed().walk().bathe()
+print(donatello.pet_health)
+print(donatello.pet_energy)
 
